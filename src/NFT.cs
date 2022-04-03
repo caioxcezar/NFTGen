2a43a85cda3d71122dc51bd4026e25a6a61b2fc7
Caio Rezende <caiocezar50@outlook.com>
2022-04-03 18:00:41 -0300

    code

---
+++ src/NFT.cs
@@ -0,0 +1,77 @@
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+
+namespace NFTGen;
+
+public class NFT
+{
+    private readonly string _input;
+    private readonly string _output;
+    private int _max;
+    private Dictionary<string, string[]> _parts;
+    public int Max => _max;
+
+    public NFT(string input, string output)
+    {
+        _input = input;
+        _output = output;
+        _max = 1;
+        
+        string[] folders = Directory.GetDirectories(_input);
+        
+        _parts = folders.Select(part => new
+        {
+            Part = new FileInfo(part).Name,
+            Files = Directory.GetFiles(part)
+        }).ToDictionary(p => p.Part, p => p.Files);
+        
+        foreach (KeyValuePair<string,string[]> part in _parts)
+            _max *= part.Value.Length;
+    }
+
+    public void GenerateRange(int qtd)
+    {
+        List<string> nfts = new();
+        if (qtd > _max) throw new Exception("Quantity is greater than the maximum amount of possibilities. ");
+        for(int i = 0; i < qtd; i++)
+        {
+            string nft;
+            do
+            {
+                nft = string.Join(',', _parts.Select((part, index) =>
+                {
+                    List<int> range = Enumerable.Range(0, part.Value.Length).ToList();
+
+                    return range[new Random().Next(range.Count)];
+                }));
+            } while (nfts.Exists(e => e == nft));
+            nfts.Add(nft);
+        }
+
+        for(int i = 0; i < nfts.Count; i++)
+        {
+            List<int> nft = nfts[i].Split(',').Select(int.Parse).ToList();
+            List<string> files = _parts.Select((part, index) => part.Value[nft[index]]).ToList();
+            Generate(files, $"{i}.png");
+        }
+    }
+
+    private void Generate(List<string> files, string name)
+    {
+        List<Image> images = files.Select(file => Image.Load(file)).ToList();
+
+        GraphicsOptions options = new GraphicsOptions();
+
+        Image? nft = images[0];
+        for (int i = 1; i < images.Count; i++)
+            nft = nft.Clone(clone => clone.DrawImage(images[i], options));
+        
+        nft.Save($"{_output}/{name}");
+        nft.Dispose();
+
+        foreach (Image image in images)
+        {
+            image.Dispose();
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,33 @@
+﻿using NFTGen;
+
+if (args.Length > 0)
+{
+    if (args.Contains("-h")) Console.WriteLine(Help());
+    else
+    {
+        try
+        {
+            NFT nft = new(args[0], args[1]);
+            nft.GenerateRange(int.Parse(args[2]));
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"An error has occurred:\n{e.Message}");
+        }
+    }
+}
+else Console.WriteLine(Help());
+
+static string Help() => @"
+NFTGen - Application for generate unique and random images
+usage: NFTGen <images-input> <images-output> <qtd_gen>
+<images-input> is a folder with subfolder where each subfolder is a part of the NFT to be made.
+<images-output> is a folder where the NFT will be saved.
+<qtd_gen> is the quantity of NFT to be generated.
+Ex.:
+  cats  
+  ├───head  
+  ├───paw  
+  ├───tail  
+commands:
+  -h: show this  ";
